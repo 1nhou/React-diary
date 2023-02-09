1ce51e77d70538887d4c0010632e5c55c118deb4
1nhou <inhouuuuu@gmail.com>
2023-02-09 23:49:43 +0900

    React를 활용한 기본 diary

---
+++ src/component/DayList.js
@@ -0,0 +1,37 @@
+// import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import useFetch from '../hooks/useFetch'
+
+export default function DayList(){
+    // costom hook을 이용한 방법
+    const days = useFetch("http://localhost:3001/days")
+
+    // 기존 방식의 json파일 읽어오기
+    // const [days,setDays] = useState([])
+    // useEffect(()=>{
+    //     fetch('http://localhost:3001/days')
+    //     .then(res => {
+    //         return res.json()
+    //     })
+    //     .then(data => {
+    //         setDays(data)
+    //     })
+    // },[])
+
+    if(days.length === 0){
+        return <span>Loading...</span>
+    }
+
+    return (
+        <>
+            <ul className='list_day'>
+                {days.map(day => (
+                    <li key={day.id}>
+                        <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+                    </li>
+                ))}
+            </ul>
+        </>
+        
+        )
+} 
